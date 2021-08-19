73ffa6451e1524a8561b34c36eb2541557b5bc70
Alwin George <alwingeorge11@gmail.com>
2021-08-19 14:14:03 +0930

    used the day.js library for easy time manipulation, implemented basic sleep cycle calculator

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 const time = document.getElementById("time")
 const sleepButton = document.getElementById("go")
 
-const currentdate = new Date(); 
+// let now = dayjs().subtract(90, 'm').format("h:mm a");
 
-const currentTime = currentdate.getHours() + ":" + currentdate.getMinutes()
+let hour;
+let min;
+let when = "am"
+let sub = 90;
 
-let finalNumb = [];
 
 sleepButton.addEventListener("click", event =>{
-    let oldTime = time.value
-    let numberTime = (oldTime.split(":"))
-    for(number of numberTime){
-       finalNumb.push(parseInt(number)) 
-    }
+    let userTime = time.value.split(":").map(Number);
     
-    console.log(currentTime)
-    console.log(oldTime)
-    console.log(finalNumb)
-    
+    convertTime(userTime)
+    let flexTime = dayjs(`2021-08-19: ${hour}:${min} ${when}`).format("h:mm a")
+    suggestions()
+    console.log(flexTime)
 })
+
+function convertTime(time){
+    if (time[0] >= 13){
+        time[0] = time[0] - 12;
+        when = "pm"
+        hour = time[0]
+        min = time[1]
+    }else{
+        when = "am"
+        hour = time[0]
+        min = time[1]
+    }
+}
+
+function suggestions(){
+    for(let i =1; i<= 6; i++){
+        flexTime = dayjs(`2021-08-19: ${hour}:${min} ${when}`).subtract(`${sub}`, 'm').format("h:mm a")
+        sub = sub + 90
+        console.log(`${flexTime} --- ${i} cycle`)
+    }
+
+}
